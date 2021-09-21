0142646d7bdca25958497d442e7a9b88f88f3bfe
Xiao Zhang <xz9rc@umsystem.edu>
2021-09-20 22:47:21 -0500

    customizing HTTP requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,8 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=2b2d8f65b287703496d4ed549efbf6e0&query=42.3605,-71.0596'
+const url = 'http://api.weatherstack.com/current?access_key=2b2d8f65b287703496d4ed549efbf6e0&query=42.3605,-71.0596&unites=f'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+    // const data = JSON.parse(response.body)
+    console.log(response.body.current.weather_descriptions[0] + response.body.current.feelslike)
 })
