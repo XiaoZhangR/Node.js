7cfef18f4a86a587cc6ffbb23015c2eb8b98aa77
Xiao Zhang <xz9rc@umsystem.edu>
2021-10-11 12:33:40 -0500

    setting up mongoose

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,7 +1,13 @@
+// /Users/zhangxiao/mongodb/bin/mongod --dbpath=/Users/zhangxiao/mongodb-data
+
 // CRUD
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+
+const { MongoClient, ObjectId } = require('mongodb')
+// const id = new ObjectId()
+// console.log(id);
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -33,23 +39,62 @@ MongoClient.connect(connectionURL, { useNewUrlParser:true, useUnifiedTopology: t
     //     console.log(result.ops);
     // })
 
-    db.collection('mydb').insertMany([
-        {
-            description: 'task1',
-            completed: true
-        }, {
-            description: 'task2',
-            completed: true
-        }, {
-            description: 'task3',
+    // db.collection('mydb').insertMany([
+    //     {
+    //         description: 'task1',
+    //         completed: true
+    //     }, {
+    //         description: 'task2',
+    //         completed: true
+    //     }, {
+    //         description: 'task3',
+    //         completed: false
+    //     }
+
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('unable');
+    //     }
+
+    //     console.log(result.ops);
+    // })
+    // db.collection('users').findOne({name: 'Pei'}, (error, user) => {
+    //     if (error) {
+    //         return console.log('Uable');
+    //     }
+    //     console.log(user);
+    // })
+
+    // db.collection('users').updateOne({
+    //     age: 31
+    // }, {
+    //     $set: {
+    //         age: 20
+    //     }
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    db.collection('mydb').updateMany({
+        completed: true
+    }, {
+        $set: {
             completed: false
         }
-
-    ], (error, result) => {
-        if (error) {
-            return console.log('unable');
-        }
-
-        console.log(result.ops);
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
     })
+
+    // db.collection('users').deleteOne({
+    //     age: 20
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
 })
